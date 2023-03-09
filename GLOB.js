f4393ba5d1ee06ac3d285df26921f537f9d3e3b1
vfInuma <Anthony_Inuma@hotmail.com>
2023-03-08 19:55:14 -0500

    maps

--- GLOB.js
+++ GLOB.js
@@ -2,7 +2,7 @@ global._STORE_ = 'curiosity';
 global._APN = 'curiosity-server-f02fe.appspot.com';
 global._SEMILLA = '87364583b873264b8723648597634';
 
-global._KEY_MAPA_GOOGLE = 'AIzaSyBVJKjuIwc-YQO2_AfefJlSG-wj4FSKlZY';
+global._KEY_MAPA_GOOGLE = 'AIzaSyCFN28IU7jTvEXWg49M1dEgg0IgxguWYX4';
 global._KEY_MAPA_HERE = 'O7k4Hi8BaFskdX3yiDflLsqw89UihPVDREcGbBAPA5A';
 
 global._ID_APP_1 = 1000001;
